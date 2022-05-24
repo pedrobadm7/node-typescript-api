3f746e5de502a61cc80514979e311a79c8d8a00d
Pedro Barros <pedrobars7a@gmail.com>
2022-05-23 21:28:27 -0300

    chore: create client api and stormGlassTest with TDD

---
+++ src/clients/stormGlass.ts
@@ -0,0 +1,12 @@
+import { AxiosStatic } from 'axios'
+
+export class StormGlass {
+  readonly stormGlassAPIParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed'
+  readonly stormGlassAPISource = 'noaa'
+  constructor(protected request: AxiosStatic) {}
+
+  public async fetchPoints(lat: number, lng: number): Promise<Record<string, unknown>> {
+    return this.request.get(`https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113602&lat=${lat}&lng=${lng}`)
+
+  }
+}
